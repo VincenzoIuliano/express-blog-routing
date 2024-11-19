c0c17dc49f386462b31c53dd6fbd3889d66efb91
VincenzoIuliano <bubucane@outlook.it>
2024-11-19 15:51:55 +0100

    EDIT: edited URI

--- routers/posts.js
+++ routers/posts.js
@@ -4,32 +4,32 @@ const router = express.router();
 // CRUD sulla risorsa post 
 
 // index 
-app.get('/posts',(req,res) => {
+app.get('/',(req,res) => {
     res.send('Ecco l\'elenco dei post!');
 })
 
 // show 
-app.get('/posts/:id',(req,res) => {
+app.get('/:id',(req,res) => {
     res.send(`Ecco il post con id: ${req.params.id}`);
 })
 
 // store 
-app.post('/posts', (req,res) => {
+app.post('/', (req,res) => {
     res.send('Creazione di un nuovo post');       
 })
 
 // update 
-app.put('/posts/:id',(req,res) => {
+app.put('/:id',(req,res) => {
     res.send(`Modifica integrale del post ${req.params.id}`)
 })
 
 // modify 
-app.patch('/posts/:id',(req,res) => {
+app.patch(':id',(req,res) => {
     res.send(`Modifica parziale del post${req.params.id}`)
 })
 
 // destroy 
-app.delete('/posts/:id',(req,res) => {
+app.delete('/:id',(req,res) => {
     res.send(`Rimozione del post ${req.params.id}`)
 })
 
