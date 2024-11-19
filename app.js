322e7568ed04e9d589c22709a01276eceba66cde
VincenzoIuliano <bubucane@outlook.it>
2024-11-19 15:32:23 +0100

    ADD: main files

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log('Hello World!')
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/',(req,res) => {
+    res.send('Ecco l\'homepage del tuo blog!')
+})
+
+app.listen(port,() => {
+    console.log(`Server listening on port: ${port}`);
+})
