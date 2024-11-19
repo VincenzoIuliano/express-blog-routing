ae8f7bc86af6194399377ef530bcd3cc68ee7629
VincenzoIuliano <bubucane@outlook.it>
2024-11-19 16:08:22 +0100

    EDIT: app.js without CRUD

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ console.log('Hello World!')
 const express = require('express')
 const app = express()
 const port = 3000
+const postsRouter = require('./routers/posts')
 
 app.use(express.static('public'))
 
 app.get('/',(req,res) => {
     res.send('Ecco l\'homepage del tuo blog!');
 })
+
+app.use('/posts', postsRouter)
 
 app.listen(port,() => {
     console.log(`Server listening on port: ${port}`);
